6ee6cc754205709bc35e678280b071956de68e92
Nikita Toropov <toropov.nikita2004@gmail.com>
2025-10-05 01:45:28 +0200

    Add simple chat client

---
+++ server/agent.py
@@ -0,0 +1,6 @@
+import os
+from load_dotenv import load_dotenv
+from google import genai
+from google.adk.agents import Agent
+
+
--- server/main.py
+++ server/main.py
@@ -1,4 +1,16 @@
+from load_dotenv import load_dotenv
+load_dotenv()
+from google import genai
+
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+
+client = genai.Client()
+
+class Message(BaseModel):
+    role: str
+    content: str
 
 app = FastAPI()
 
@@ -6,3 +18,10 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+
+@app.post("/chat")
+def ask_ai(messages: list[Message]):
+    print(messages)
+    response = client.models.generate_content(model="gemini-2.5-flash", contents="Explain how AI works in a few words")
+    return response.text
